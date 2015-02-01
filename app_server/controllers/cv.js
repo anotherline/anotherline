cc8660f2caf00baee1c1259352547ff43f0c9384
Chris Lindgren <chris.a.lindgren@gmail.com>
2015-02-01 10:12:01 -0600

    started revising traditional template

--- app_server/controllers/cv.js
+++ app_server/controllers/cv.js
@@ -1045,7 +1045,663 @@ module.exports.cvDropDown = function(req, res){
 
 /* GET 'tabular template cv' page */
 module.exports.cvTraditional = function(req, res){
-  res.render('cv-template-traditional', { title: 'My CV' });
+  // res.render('cv-template-traditional', { title: 'My CV' });
+  res.render('cv-template-traditional', { 
+    title: 'Dropdown Template Test',
+    cv: {
+      general: {
+        given:'Chris',
+        family:'Lindgren',
+        mi:'A.',
+        title:'PhD Student',
+        university:'University of Minnesota',
+        picture:'http://www.clindgrencv.com/assets/img/blog-header-200.jpg',
+        email:'lindg250@umn.edu',
+        phone:'',
+        website:'http://www.clindgrencv.com',
+        location: {
+          address1: 'Nolte Center',
+          address2: '',
+          city:'Minneapolis',
+          state:'MN',
+          postalCode:'56082',
+          countryCode:''
+        }
+      },
+      education: {
+        phd: {
+          degCheck:true,
+          degree:'PhD',
+          title:'Student',
+          institution: 'University of Minnesota',
+          program: 'Rhetoric of Science and Technical Communication',
+          programURL: 'http://writingstudies.umn.edu/grad/phd/',
+          startDate: '',
+          endDate: '2017',
+          expectedDate:'Spring 2017',
+          thesisTitle: '',
+          committee:
+          {
+            hasCommittee: false,
+            advisor:'Christina Haas'
+          }
+        },
+        masters: {
+          degCheck:true,
+          degree:'MA',
+          title:'',
+          institution: 'North Dakota State University',
+          program:'Composition Studies',
+          startDate: 'Fall 2009',
+          endDate: 'Summer 2012',
+          expectedDate:'',
+          thesisTitle: '\"Where\’s the Revolution? From \'Code Year\' to the Continuum of Proceduracy\"',
+          committee:
+          {
+            hasCommittee: true,
+            advisor:'Kevin Brooks',
+            member1:'Andrew Mara',
+            member2:'Kelly Sassi',
+            outside:'Brain Slator'
+          }
+        },
+        undergrad: {
+          degCheck:true,
+          degree:'BA',
+          institution: 'North Dakota State University',
+          program:'English Studies',
+          startDate: 'Fall 1999',
+          endDate: 'Summer 2004',
+          advisor:'Kevin Brooks'
+        }
+      },
+      appointments: {
+        university: 
+        [{
+          name: 'University of Minnesota',
+          current: true,
+          courses:
+          [{
+            position: 'Graduate Instructor',
+            deptShort: 'WRIT',
+            courseNum: '3577w',
+            course: 'Rhetoric, Technology, and the Internet',
+            website: '',
+            semesters: ['Fall 2014'],
+            summary: '',
+            highlights: [
+              ''
+            ]
+          },
+          {
+            position: 'Graduate Instructor',
+            deptShort: 'WRIT',
+            courseNum: '1301',
+            course: 'University Writing',
+            website: '',
+            semesters: ['Fall 2012','Fall 2013'],
+            summary: '',
+            highlights: [
+              ''
+            ]
+          }],
+          research:
+          [{
+            position: 'RA',
+            department: 'Computer Science and Engineering',
+            project: 'Writing-Enriched Curriculum',
+            website: '',
+            semesters: ['Spring 2013','Summer 2013'],
+            summary: '',
+            highlights: [
+              ''
+            ]
+          }]
+        },
+        {
+          name: 'North Dakota State University',
+          current: false,
+          courses:
+          [{
+            position: 'Graduate Instructor',
+            deptShort: 'ENG',
+            courseNum: '120',
+            course: 'First-Year Writing',
+            website: '',
+            semesters: ['Fall 2009','Spring 2010','Fall 2010','Spring 2011','Fall 2011','Spring 2012'],
+            summary: '',
+            highlights: [
+              ''
+            ]
+          }],
+          research: 
+          [{
+            position: 'RA',
+            department: 'English Education',
+            project: 'Common Core Curriculum and National Writing Project',
+            website: '',
+            semesters: ['Spring 2011'],
+            summary: '',
+            highlights: [
+              ''
+            ]
+          }]
+        }]
+      },
+      publications: {
+        books: [{
+          forthcoming: false,
+          title: 'Understanding computer programming as an embodied writing practice',
+          role: true,
+          authors: [{
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          editors: [{
+            number: '',
+            given: '',
+            family: ''
+          }],
+          press: 'MIT Press',
+          pressLoc: {
+            city:'Cambridge',
+            state:'MA'
+          },
+          date: '2020'
+        },
+        {
+          forthcoming: true,
+          title: 'Building a technical discourse: Writing code in non-compilable spaces',
+          role: false,
+          authors: [{
+            number: '',
+            given: '',
+            family: ''
+          }],
+          editors: [{
+            number: 1,
+            given: 'ChrisEditor',
+            family: 'Lindgren'
+          }],
+          press: 'MIT Press',
+          pressLoc: {
+            city:'Cambridge',
+            state:'MA'
+          },
+          date: '2025'
+        }],
+        chapters: [
+        {
+          forthcoming: false,
+          title: 'Tackling a fundamental problem: Using digital labs to build a smarter computing culture',
+          collection:'Rhetoric and the Digital Humanities',
+          authors: [{
+            number: 1,
+            given: 'Kevin',
+            family: 'Brooks'
+          },
+          {
+            number: 2,
+            given: 'Chris',
+            family: 'Lindgren'
+          },
+          {
+            number: 3,
+            given: 'Matthew',
+            family: 'Warner'
+          }],
+          editors: [
+          {
+            number: 1,
+            given:'Jim',
+            family:'Ridolfo'
+          },
+          {
+            number:2,
+            given:'William',
+            family:'Hart-Davidson'
+          }],
+          press: 'University of Chicago',
+          pressLoc: {
+            city:'Chicago',
+            state:'IL'
+          },
+          date: '2015',
+          pgNums: '140-165',
+          abstract: '',
+          assets:
+          {
+            exists: false,
+            permaLink: '',
+            isbn:'',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            'Rhetoric','Digital Humanities'
+          ],
+          tags: [
+            ''
+          ]
+        },
+        {
+          forthcoming: true,
+          title: 'Responding to the coding crisis: From \'Code Year\' to computational literacy',
+          collection:'Strategic Discourse: The Politics of (New) Literacy Crises',
+          authors: [{
+            number: 1,
+            given: 'Kevin',
+            family: 'Brooks'
+          },
+          {
+            number: 2,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          editors: [
+          {
+            number: 1,
+            given:'Lynn',
+            family:'Lewis',
+            mi: 'C'
+          }],
+          press: 'Computers and Composition Digital Press',
+          pressLoc: {
+            city:'Salt Lake City',
+            state:'UT'
+          },
+          date: '2015',
+          pgNums: '30-55',
+          abstract: '',
+          assets:
+          {
+            exists: true,
+            permaLink: 'http://www.notareallink.com',
+            isbn:'',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            'Rhetoric','Literacy Studies'
+          ],
+          tags: [
+            ''
+          ]
+        },
+        {
+          forthcoming: true,
+          title: 'Utopian laptop initiatives: From technological deism to object-oriented rhetoric',
+          collection:'Writing Posthumanism, Posthuman Writing',
+          authors: [{
+            number: 1,
+            given: 'Byron',
+            family: 'Hawk'
+          },
+          {
+            number: 2,
+            given: 'Chris',
+            family: 'Lindgren'
+          },
+          {
+            number: 3,
+            given: 'Andrew',
+            family: 'Mara'
+          }],
+          editors: [
+          {
+            number: 1,
+            given:'Sidney',
+            family:'Dobrin'
+          }],
+          press: 'Parlor Press',
+          pressLoc: {
+            city:'Anderson',
+            state:'SC'
+          },
+          date: '2015',
+          pgNums: '1-38',
+          abstract: '',
+          assets:
+          {
+            exists: false,
+            permaLink: '',
+            isbn:'',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            'Rhetoric','Object-Oriented Rhetoric'
+          ],
+          tags: [
+            ''
+          ]
+        }],
+        journals: [
+        {
+          forthcoming:false,
+          title: 'The rhetorical nature of data structures',
+          authors: [
+          {
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          journal: 'Journal of Business and Technical Communication',
+          volume: '56',
+          date: '2014',
+          pgNums: '45-92',
+          abstract: '',
+          assets:
+          {
+            exists: false,
+            permaLink: '',
+            doi:'',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            ''
+          ],
+          tags: [
+            ''
+          ]
+        },
+        {
+          forthcoming: true,
+          title: 'Regulation and distributed ethos in U.S. online news coverage during the 2014 #Ferguson Protests',
+          authors: [
+          {
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          },
+          {
+            number: 2,
+            given: 'Laura',
+            family: 'Lindgren'
+          }],
+          journal: 'Journal of Business and Technical Communication',
+          volume: '57',
+          date: '2015',
+          pgNums: '45-92',
+          abstract: '',
+          assets:
+          {
+            exists: true,
+            permaLink: '',
+            doi:'',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            ''
+          ],
+          tags: [
+            ''
+          ]
+        }],
+        web: [{
+          forthcoming:false,
+          title: 'Regulation and distributed ethos in China\'s online news coverage during the 2014 Hong Kong Protests',
+          authors: [
+          {
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          journal: 'Kairos',
+          volume:'21',
+          issue:'1',
+          date: '2015',
+          abstract: '',
+          assets:
+          {
+            exists: true,
+            permaLink: 'http://www.kairos.edu/thislinkisnotreal',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            ''
+          ],
+          tags: [
+            ''
+          ]
+        }],
+        coursework: [{
+          forthcoming:false,
+          title: 'Writing with/in the cloud: An autoethnography about the avocations, invocations & evocations of Twitter\'s API',
+          authors: [
+          {
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          course: 'Writing in the Cloud',
+          date: 'Fall 2013',
+          abstract: 'Application Programming Interfaces (APIs) are not written for machines (Tulach 2008). Organizations and companies write APIs primarily for composite human audiences: web developers with a wide range of skill levels and purposes to engage with their data and services on the Web. APIs have become increasingly valuable resources for companies like Twitter, who thrive on the creativity and productivity of third-party developers to create new Web apps that utilize their dynamic sources of user-generated data. Using a Vygotskian theory and autoethnography methodological approach, I explore my ongoing situated code-writing practices and learning as interlaced with the encoded sociocultural histories wound up within the process of writing with the Twitter API.',
+          assets:
+          {
+            exists: true,
+            permaLink: 'http://api.clindgrencv.com/',
+            linkTxt: '',
+            img: '',
+            altAttr: ''
+          },
+          field: [
+            ''
+          ],
+          tags: [
+            ''
+          ]
+        }],
+      },
+      presentations: {
+        conference: [{
+          date:"Sep. 2014",
+          authors: [
+          {
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          title:"The dynamic materialities of programming practices",
+          conference:"Association for Computing Machinery: Special Interest Group on the Communication of Design",
+          location:
+          {
+            city:"Colorado Springs",
+            state:"CO",
+            country: ""
+          },
+          assets:
+          {
+            exists: true,
+            presLink:"https://docs.google.com/presentation/d/1ichOzca0O5WjdG8QrnXbUoxs-syMhKbfc2rJnwq5VWo/pub?start=false&loop=false&delayms=20000#slide=id.p",
+            linkTxt:"Link to slides and script."
+          },
+          invited: true
+        }],
+        workshop: [{
+          date: "2015",
+          leaders: [{
+            number: 1,
+            given: 'William',
+            family: 'Hart-Davidson'
+          },
+          {
+            number: 2,
+            given: 'Ryan',
+            family: 'Omizo'
+          }],
+          title: "Building \'Soph\'ware: Modeling theoretical approaches to technical and professional writing with computational methods",
+          workshop:"Rhetoric Society of America Summer Institute",
+          role: "Participant",
+          location:
+          {
+            city:"Madison",
+            state:"WI",
+            country: ""
+          },
+          assets:
+          {
+            exists: false,
+            presLink:"",
+            linkTxt:""
+          },
+          invited: false
+        }],
+        department: [{
+          date:"Oct. 2014",
+          authors: [
+          {
+            number: 1,
+            given: 'Chris',
+            family: 'Lindgren'
+          }],
+          title: "The social nature of programming practices: Expanding the notion of the \'technical\'",
+          event: "Department of Writing Studies: Research Parlor",
+          location:
+          {
+            city: "Minneapolis",
+            state: "MN"
+          },
+          assets:
+          {
+            exists: false,
+            presLink: "",
+            linkTxt: ""
+          },
+          invited: true
+        }]
+      },
+      service: {
+        editorial: [{
+          role: "Reviewer",
+          journal: "Written Communication"
+        }],
+        groups: [{
+          date:"",
+          title:"",
+          organization:""
+        }],
+        committees: [{
+          semesters: ['Fall 2014','Spring 2015'],
+          title:"Code work: Exploring Digital Studies through code",
+          role:"Convener",
+          organization:"Institute for Advanced Study: Research and Creative Collaborative",
+          university: "University of Minnesota",
+          invited: true
+        }]
+      },
+      grants: [{
+        grantees: [
+        {
+          number: 1,
+          given: "Justin",
+          family: "Schell"
+        },
+        {
+          number: 2,
+          given: "Chris",
+          family: "Lindgren"
+        },
+        {
+          number: 3,
+          given: "Alison",
+          family: "Link"
+        },
+        {
+          number: 4,
+          given: "Jeffrey",
+          family: "Kerzner"
+        }],
+        name: 'Code work: Exploring Digital Studies through code',
+        sponsor:{
+          subExists: true,
+          principal: 'University of Minnesota',
+          subordinate: 'Institute for Advanced Study: Research and Creative Collaborative'
+        },
+        startDate:'2014',
+        endDate:'2015',
+        amount:'10,000'
+      }],
+      awards: {
+        fellowship: [{
+          name: "Social Media Fellowship",
+          sponsor:{
+            subExists: false,
+            principal:"Rhetoric Society of America",
+            subordinate:""
+          },
+          date:{
+            semExists: false,
+            year:"2014",
+            semester:""
+          }
+        }],
+        award: [{
+          name:"Teaching Excellence Award",
+          sponsor:{
+            subExists: false,
+            principal:"University of Minnesota",
+            subordinate:"Department of Writing Studies"
+          },
+          date:{
+            semExists: true,
+            year:"2014",
+            semester:"Fall"
+          }
+        }]
+      },
+      affiliations:[{
+        name:"Rhetoric Society of America",
+        startDate:"",
+        endDate:""
+      }],
+      proficiencies: [{
+        audio:[
+          "Ableton Live", "Adobe Audition", "Audacity", "MLRV"
+        ],
+        video: [
+          "Novacut", "Kdenlive", "iMovie", "Modul8"
+        ],
+        graphics: [
+          "Adobe InDesign", "Photoshop", "Fireworks", "Kira", "Inkscape" 
+        ],
+        office: [
+          "Microsoft", "LibreOffice" 
+        ],
+        courseManagement: [
+          "Blackboard", "Moodle", "Wordpress as a LMS" 
+        ],
+        other: [{
+          name:"Version Control",
+          platforms: [
+            "Github", "FTP"
+          ]
+        }]
+      }],
+      languages: [{
+        language: "Mandarin",
+        fluency: "Two years of coursework"
+      },
+      {
+        language: "Javascript",
+        fluency: "Intermediate"
+      },
+      {
+        language: "Python",
+        fluency: "Intermediate"
+      }]
+    }
+  });
 };
 
 /* GET 'Change template' page */
